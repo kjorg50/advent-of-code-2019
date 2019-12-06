448b9ba79ac845ef76dc58a844052701c8d2631e
Kyle Jorgensen <kyle.jorgensen@workday.com>
2019-12-05 18:05:47 -0800

    Day01 part 2 complete

--- golang/01/day01.go
+++ golang/01/day01.go
@@ -9,29 +9,62 @@ import (
 )
 
 func main() {
-  if len(os.Args) != 3 {
-    fmt.Println("Usage: go run day01.go -- <filename>")
+  if len(os.Args) != 4 {
+    fmt.Println("Usage: go run day01.go -- part[1|2] <filename>")
+    os.Exit(1)
   }
-  filename := os.Args[2]
+  part := os.Args[2]
+  filename := os.Args[3]
 
   file, err := os.Open(filename)
   if err != nil {
       log.Fatal(err)
   }
   defer file.Close()
-
-  result := 0
   scanner := bufio.NewScanner(file)
+
+  switch part {
+  case "part1":
+    part1(scanner)
+  case "part2":
+    part2(scanner)
+  default:
+    fmt.Println("You must select 'part1' or 'part2'")
+    os.Exit(1)
+  }
+}
+
+func part1(scanner *bufio.Scanner) {
+  result := 0
   for scanner.Scan() {
     if i, err := strconv.Atoi(scanner.Text()); err == nil {
-      mass := (i / 3) - 2
+      mass := i / 3 - 2
       result += mass
     }
   }
 
   if err := scanner.Err(); err != nil {
-      log.Fatal(err)
+    log.Fatal(err)
   }
 
   fmt.Println(result)
+}
+
+func part2(scanner *bufio.Scanner) {
+  result := 0
+  for scanner.Scan() {
+    if i, err := strconv.Atoi(scanner.Text()); err == nil {
+      result += calcRemainingFuel(i)
+    }
+  }
+  fmt.Println(result)
+}
+
+func calcRemainingFuel(x int) int {
+  neededFuel := x / 3 - 2
+  if (neededFuel) < 1 {
+    return 0
+  } else {
+    return neededFuel + calcRemainingFuel(neededFuel)
+  }
 }
--- golang/template.go
+++ golang/template.go
@@ -8,24 +8,51 @@ import (
 )
 
 func main() {
-  if len(os.Args) != 3 {
-    fmt.Println("Usage: go run day01.go -- <filename>")
+  if len(os.Args) != 4 {
+    fmt.Println("Usage: go run dayXX.go -- part[1|2] <filename>")
+    os.Exit(1)
   }
-  filename := os.Args[2]
+  part := os.Args[2]
+  filename := os.Args[3]
 
   file, err := os.Open(filename)
   if err != nil {
-      log.Fatal(err)
+    log.Fatal(err)
   }
   defer file.Close()
-
   scanner := bufio.NewScanner(file)
+
+  switch part {
+  case "part1":
+    part1(scanner)
+  case "part2":
+    part2(scanner)
+  default:
+    fmt.Println("You must select 'part1' or 'part2'")
+    os.Exit(1)
+  }
+}
+
+func part1(scanner *bufio.Scanner) {
   for scanner.Scan() {
-      // do stuff
-      fmt.Println(scanner.Text())
+    // get line of text with scanner.Text()
+
   }
 
   if err := scanner.Err(); err != nil {
-      log.Fatal(err)
+    log.Fatal(err)
   }
+
+}
+
+func part2(scanner *bufio.Scanner) {
+  for scanner.Scan() {
+    // get line of text with scanner.Text()
+
+  }
+
+  if err := scanner.Err(); err != nil {
+    log.Fatal(err)
+  }
+  
 }
